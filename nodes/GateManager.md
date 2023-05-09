28ecd6a057654d7b22e70bc8d13d381174d22f3f
Jon Little <burundiocibu@gmail.com>
2023-05-09 17:49:32 -0500

    working pwm

--- nodes/GateManager.md
+++ nodes/GateManager.md
@@ -1,9 +1,7 @@
-# GateManager 
-## Hardware
-### Renogy Rover
-#### RJ-12 Jack 
+# Hardware
+## Renogy Rover/Wanderer
+### RJ-12 Jack 
 Talks modbus protocol. See https://www.dropbox.com/s/8itoxh8c3uf4ncq/ROVER%20MODBUS.DOCX?dl=0 for the specs
-See https://github.com/corbinbs/solarshed for some python code that talks to one
 
 Using a standard RJ-12 cable (blue,yellow,green,red,black,white)
 blue is the rover's RxD
@@ -11,26 +9,108 @@ yellow is the rover's TxD
 green is the rover's ground
 black, is rover +12V
 
-### Mule gate controller
+### Main proto board overview
+This is a prototyping board with DIN mounting brackets. On this board are the following
 
-### Adafruit LoRa feather
-#### Connectors / Nets
-J1-1 (PCB1)level converter ground; to rover modbus ground (pin 3 on jack) (grey on RJ12 cable)
-J1-2 (PCB2) Level converter "ttl RxD"; to rover modbus TxD (pin 2 on jack) (green on RJ12 cable)
-J1-3 (PCB3) Level converter "ttl TxD"; to rover modbus RxD (pin 1 rover jack) (blue on JR12 cable)
+* Adafruit LoRa feather module with an ATMega32u4 and RFM9x LoRa radio https://www.adafruit.com/product/3078
+* Generic RS232-TTL level converter to talk to the renology charge contoller 
+* Pololu brushed dc motor driver https://www.pololu.com/product/2999/resources
+   * TB67H420 motor driver: https://toshiba.semicon-storage.com/info/TB67H420FTG_datasheet_en_20201016.pdf?did=59110&prodName=TB67H420FTG
+   * HBMODE is strapped hight to run both  channels in parallel
+   * all input is on PWMA, INA1, and INA2. PWMB, INB1, and INB2 are ignored
+   * Connect to feather: PWMA, INA1, INA2, LO1, LO2
 
-J2-1 (PCB6), ground
-J2-2 (PCB7), feather pin 21, relay 1, (gate safe) 
-J2-3 (PCB8), feather pin 22, relay 2, (gate open)
-J2-4 (PCB9), feather pin 23, relay 3, (poe power enable)
-J2-5 (PCB10), ground
-J2-6  (PCB11), feather 20, relay 4, (gate power)
-J2-7  (PCB12), feather 19, tbd
-J2-8  (PCB13), feather 18 , tbd
+### Modules
 
-J3-1 (PCB22) ground
-J3-2 (PCB22), 5VDC, to feather USB (VBUS), to 5VDC converter
-J3-3 (PCB22), Vbat-sla, to 33k/10k resistive divider (3.3:1), to feather pin A11
+#### Feather 32u4 with RFM9x
+ATmega32u4 @ 8 MHz, 32k flash, 2k ram
 
-### ttl relay board
+Pin numbers are arduino pin numbers or silkscreen
+
+#### Mainboard
+
+Numbers are columns, 74 is far right
+
+Letters are rows, A row with connector blocks
+
+Feather numbers are silkscreen
+
+| PWB  | Feather | Connections
+|------|---------|------------
+| C59  | 2/SDA   | Encoder signal, w 5 k pullup to +5 * "filter", PWB B70
+| C60  | 3/SCL   | TB67H420 LO2, PWB B58
+| C61  | 5       | TB67H420 PWMA, TIMER3A, PWB B53
+| C62  | 6/A7    | TB67H420 LO1, PWB B57
+| C63  | 9/A9    | Feather Vbat via 1:1 divider
+| C64  | 10/A10  | TB67H420 INA1, PWB B51
+| C65  | 11      | TB67H420 INA2, PWB B52
+| C66  | 12/A11  | System Vload, via 33k:10k divider, PWB B66
+| C67  | 13      | Feather LED
+| C68  | Vbus    | 5VDC buss
+| C69  | EN      |
+| C70  | Vbat    | Feather vbat
+|------|---------|-------------
+| D59  | DIO1    | 
+| D60  | 1/TxD1  | TTL-RxD
+| D61  | 0/RxD1  | TTL-TxD, PWB C26
+| D62  | MISO    | RMF9x SPI
+| D63  | MOSI    | RMF9x SPI
+| D64  | SCK     | RMF9x SPI
+| D65  | A5      |
+| D66  | A4      |
+| D67  | A3      | Driveway remote sensor in
+| D68  | A2      | Wireless remote receiver in, PWB A55
+| D69  | A1      | To Relay in, Relay D1, for gate lock, PWB C53
+| D70  | A0      | Encoder signal, PWB C59
+| D71  | GND     | Gnd, PWB B2
+| D72  | AREF    |
+| D73  | 3.3V    | 
+| D74  | RST     |
+
+| PWB  | Relay   | Connections
+|------|---------|------------
+| C48  | 5V      | 5VDC buss
+| C49  | Gnd     | Gnd
+| C53  | D1      | Feather A1, PWB D69
+|      | NO      | Act Yel, PWB A63
+|      | COM     | Vload buss
+
+| PWB  | TB67H420| Connections
+|------|---------|------------
+| B48  | VM      | 
+| B49  | Gnd     | Gnd
+| B50  | Vcc     | 
+| B51  | INA1    | Feather 10, PWB C64
+| B52  | INA2    | Feather 11, PWB C65
+| B53  | PWMA    | Feather 3, PWB C60
+| B54  | INB1    | 
+| B55  | INB2    | 
+| B56  | PWMB    | 
+| B57  | LO1     | Feather 6, PWB C62
+| B58  | LO2     | Feather 5, PWB C61
+| B59  | VrefA   |
+|      | A+      | Act Red, PWB A65
+|      | A-      | Act Blk, PWB A63
+|      | Vin     | Vload buss
+|      | Gnd     | Gnd buss
+
+| PWB  | TTL     | Connections
+|------|---------|------------
+| C26  | RxD     | Feather 1, PWB D60
+| C27  | TxD     | Feather 0, PWB D61
+| C28  | Gnd     | Gnd
+| C29  | Vcc     | 5VDC buss
+
+| PWB  | Terminals | Connections
+|------|-----------|------------
+| A74  | 12VDC     | Charge controller Vload +, PWB 12VDC buss
+| A72  | Gnd       | Charge controller Vload -, PWB Gnd buss, B2, B3
+| A70  | Act Wht   | Encoder signal, PWB B70, Acutator white
+| A67  | Act Grn   | Encoder ground
+| A65  | Act Red   | Motor +, TB67H420 A+, Actuator Red
+| A63  | Act Blk   | Motor -, TB67H420 A-, Actuator Black
+| A60  | Act Blu   | Lock -, Gnd
+| A58  | Act Yel   | Lock +, Relay NO
+| A55  | Rcvr      | Remote receiver signal, Feather A2, PWB D68
+| A53  | Rcvr Gnd  | Remote receiver Gnd, Gnd, Gnd buss
 
--- nodes/src/driveway_gate.cpp
+++ nodes/src/driveway_gate.cpp
@@ -7,12 +7,11 @@
 
 #include "renogyrover.hpp"
 
+
 // for feather32u4
-#define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 7 // This is from the pinout image, not the text
-
-// Change to 434.0 or other frequency, must match RX's freq!
+#define RFM95_CS 8
 #define RF95_FREQ 915.0
 
 // Singleton instance of the radio driver
@@ -23,70 +22,68 @@ RHReliableDatagram manager(rf95, NODE_ADDRESS);
 
 #define FEATHER_VBAT 9  // connected to feather Vbatt through a 1/2 divider network
 
-#define GATE_SAFE 21 // GPIO output to relay
-#define GATE_OPEN 22 // GPIO output to relay
-#define POE_ENABLE 23 // GPIO output to relay
-#define GATE_ENABLE 20  // gate controler on/off
+#define GATE_LOCK 21 // GPIO output to relay
 
-#define GATE_VBAT A11 // (w divider)
+#define ROVER_VLOAD A11 // (w divider)
+
+// I/O to TB67H420 motor driver
+// Note that HBMODE is jumpered high on board.
+#define TB67_PWMA 5
+#define TB67_INA1 10
+#define TB67_INA2 11
+#define TB67_LO1 6
+#define TB67_LO2 3
+
+
+uint16_t pwm_top = 0;
+float pwm_duty = 0;
+float pwm_freq = 0;
+
+// A pulsecounting encoder on the linear motor
+// one is for an analog read and the other is to generate interrupts
+// They are both the same signal coming from the encoder.
+#define PULSE_V A0
+#define PULSE_I 2
 
 // 5 to 23 dB on this device
-int txpwr = 23;
+int txpwr = 20;
 
 int tx_rtt = 0;
 int gate_position = 0;
-int poe_enable = 0;
-int gate_enable = 1;
 int rover_load_enable = 1;
 
 RenogyRover rover(Serial1, 1);
 
-long dt(unsigned long start_time)
-{
-    unsigned long now = millis();
-    if (now < start_time)
-        return now - start_time + 0x10000;
-    else
-        return now - start_time;
-}
 
-// returns uptime in seconds
-long uptime()
-{
-    static uint32_t last_millis = 0;
-    static uint32_t wraps = 0;
-    uint32_t ms = millis();
-    if (ms < last_millis)
-        wraps++;
-    return ms / 1000 + wraps * 4294967; // 4294967 = 2^32/1000
-}
+
 
 void setup()
 {
-    pinMode(RFM95_RST, OUTPUT);
-    digitalWrite(RFM95_RST, HIGH);
-
-    pinMode(GATE_OPEN, OUTPUT);
-    digitalWrite(GATE_OPEN, LOW);
-    pinMode(GATE_SAFE, OUTPUT);
-    digitalWrite(GATE_SAFE, LOW);
-
-    pinMode(GATE_ENABLE, OUTPUT);
-    digitalWrite(GATE_ENABLE, gate_enable);
-
-    pinMode(POE_ENABLE, OUTPUT);
-    digitalWrite(POE_ENABLE, poe_enable);
-
-    // modbus
-    Serial1.begin(9600);
-
     // Console
     Serial.begin(115200);
     //while (!Serial) delay(10);
     Serial.println("LoRaNet node");
 
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
+
+    pinMode(GATE_LOCK, OUTPUT);
+    digitalWrite(GATE_LOCK, LOW);
+
+    pinMode(TB67_PWMA, OUTPUT);
+    digitalWrite(TB67_PWMA, LOW);
+    pinMode(TB67_INA1, OUTPUT);
+    digitalWrite(TB67_INA1, LOW);
+    pinMode(TB67_INA2, OUTPUT);
+    digitalWrite(TB67_INA2, LOW);
+    pinMode(TB67_LO1, INPUT);
+    pinMode(TB67_LO2, INPUT);
+
     if (!manager.init())
         Serial.println("manager init failed");
+
+    // modbus
+    Serial1.begin(9600);
 
     // manual reset the radio
     if (false)
@@ -116,11 +113,47 @@ void setup()
 
     rf95.setTxPower(txpwr, false);
 
-    // Always start with the rover on
-    if (!rover.load_on())
-        rover.load_on(1);
+    if (rover.load_on() != rover_load_enable)
+        rover.load_on(rover_load_enable);
 
+    // PWM is being generated on Arduino pin 5
+    // 32u4 pin 31, port C6, OC.3A
+    // Using Timer 3 output compare A
+
+    // See tables 14-3 and 14-5
+    // COM3A[1:0] = 10 - Clear OC3A on compare match, Set OC3A at TOP
+    // WGM3[3:0] = 111 - Fast PWM, 10-bit, TOP=0x03FF TOP TOP
+    TCCR3A = _BV(COM3A1) | _BV(WGM32) | _BV(WGM31) | _BV(WGM30);
+    
+    // Table 14-6
+    // CS0[2:0] = 011, /64 prescaler, about 61 Hz
+    TCCR3B = _BV(CS31) | _BV(CS30);
+
+    pwm_top = 0x3ff;
+    pwm_freq = 3910.0 / 64; // 16 ms/cycle
+    OCR3A = (pwm_duty/100) * pwm_top;
 }
+
+long dt(unsigned long start_time)
+{
+    unsigned long now = millis();
+    if (now < start_time)
+        return now - start_time + 0x10000;
+    else
+        return now - start_time;
+}
+
+// returns uptime in seconds
+long uptime()
+{
+    static uint32_t last_millis = 0;
+    static uint32_t wraps = 0;
+    uint32_t ms = millis();
+    if (ms < last_millis)
+        wraps++;
+    return ms / 1000 + wraps * 4294967; // 4294967 = 2^32/1000
+}
+
 
 
 String runtime()
@@ -154,8 +187,11 @@ void send_update(uint8_t dest)
 {
     // LiIon pack directly connected to feather
     float feather_vbat = analogRead(FEATHER_VBAT) * 2 * 3.3 / 1024;
-    // Gel-cell that powers the gate controller board
-    float gate_vbat = analogRead(GATE_VBAT) * 4.29 * 3.3/1024;
+    // LiFePo4 battery managed by the renogy mgr
+    float gate_vbat = analogRead(ROVER_VLOAD) * 4.29 * 3.3/1024;
+
+    float bv = rover.battery_voltage();
+    int rc = bv<100;
 
     String msg;
     msg += String("gp:") + String(gate_position);
@@ -165,15 +201,12 @@ void send_update(uint8_t dest)
     msg += String(",snr:") + String(rf95.lastSNR());
     msg += String(",txpwr:") + String(txpwr);
     msg += String(",ut:") + String (uptime());
-    msg += String(",poe:") + String(poe_enable);
-    msg += String(",ge:") + String(gate_enable);
     if (tx_rtt > 0)
         msg += String(",rtt:") + String(tx_rtt);
+    msg += String(",rc:") + String(rc);
     send_msg(dest, msg);
 
-
-    float bv = rover.battery_voltage();
-    if (bv < 100)
+    if (rc)
     {
         msg = String("bv:") + String(bv);
         msg += String(",bc:") + String(rover.battery_current());
@@ -196,38 +229,7 @@ void send_update(uint8_t dest)
 
 void set_gate_position(int pos, int dest)
 {
-    if (pos > 0)
-    {
-        digitalWrite(GATE_OPEN, 1);
-        delay(100);
-        digitalWrite(GATE_SAFE, 1);
-        delay(10);
-        digitalWrite(GATE_OPEN, 0);
-        gate_position = 100;
-    }
-    else
-    {
-        digitalWrite(GATE_OPEN, 0);
-        digitalWrite(GATE_SAFE, 0);
-        gate_position = 0;
-    }
     send_msg(dest, String("gp:") + String(gate_position));
-}
-
-
-void set_poe_enable(int poe, int dest)
-{
-    poe_enable = poe?1:0;
-    digitalWrite(POE_ENABLE, poe_enable);
-    send_msg(dest, String("poe:") + String(poe_enable));
-}
-
-
-void set_gate_enable(int ge, int dest)
-{
-    gate_enable = ge?1:0;
-    digitalWrite(GATE_ENABLE, gate_enable);
-    send_msg(dest, String("ge:") + String(gate_enable));
 }
 
 
@@ -255,18 +257,26 @@ void loop()
 
         if (msg=="GO")       set_gate_position(100, from);
         else if (msg=="GC")  set_gate_position(0, from);
-        else if (msg=="GE1") set_gate_enable(1, from);
-        else if (msg=="GE0") set_gate_enable(0, from);
-        else if (msg=="E1")  set_poe_enable(1, from);
-        else if (msg=="E0")  set_poe_enable(0, from);
         else if (msg=="R1")  set_rover_load(1, from);
         else if (msg=="R0")  set_rover_load(0, from);
     }
 
-    const long update_rate = 15000; // ms
+
+    const long update_rate = 5000; // ms
     if (last_update == 0 || dt(last_update) > update_rate)
     {
+        static float delta = 5;
+        static int dir = -1;
+        if (pwm_duty <= 0)
+            dir = 1;
+        else if (pwm_duty >= 100)
+            dir = -1;
+        pwm_duty += dir * delta;
+        OCR3A = (pwm_duty/100) * pwm_top;
         send_update(0);
+        Serial.print(runtime() + " pwm_duty:");
+        Serial.println(pwm_duty);
         last_update += update_rate;
     }
 }
+
